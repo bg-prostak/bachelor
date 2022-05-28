c493370b8310b2cb196977d209f44bc3ce1b8527
LitvinVK <LitvinVK@yandex.ru>
2022-05-28 16:46:17 +0300

    add search + add musician photos

--- js/script.js
+++ js/script.js
@@ -20,3 +20,23 @@ function hoverOffOrange(){
     document.getElementById("text-muz").classList.remove("active");
     document.getElementById("text-man").classList.remove("active");
 }
+
+document.querySelector('#musicians__body').oninput = function () {
+    let val = this.value.trim();
+    let elasticItems = document.querySelectorAll('.musicians__body');
+    if (val != '') {
+        elasticItems.forEach(function (elem) {
+            if (elem.innerText.toLowerCase().search(val.toLowerCase()) == -1) {
+                elem.classList.add('hide');
+            }
+            else {
+                elem.classList.remove('hide');
+            }
+        });
+    }
+    else {
+        elasticItems.forEach(function (elem) {
+            elem.classList.remove('hide');
+        });
+    }
+}
