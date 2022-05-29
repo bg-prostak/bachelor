74467d81f93092589cc80a054bb35837583401af
LitvinVK <LitvinVK@yandex.ru>
2022-05-29 21:35:32 +0300

    add musician page + hovers

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
-function hoverOnBlue(){
-    document.getElementById("welcome__button-man").classList.add("active");
+let coll = document.getElementsByClassName('equipment__more');
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    })
 }
-function hoverOffBlue(){
-    document.getElementById("welcome__button-man").classList.remove("active");
-}
+
 function hoverOnOrange(){
-    document.getElementById("welcome__button-muz").classList.add("active");
     document.getElementById("welcome__body").classList.add("active");
     document.getElementById("title-muz").classList.add("active");
     document.getElementById("title-man").classList.add("active");
     document.getElementById("text-muz").classList.add("active");
     document.getElementById("text-man").classList.add("active");
+    document.getElementById("header__stick").classList.add("active");
+    document.getElementById("musicians__body").classList.add("active");
+    document.getElementById("musicians").classList.add("active");
 }
 function hoverOffOrange(){
-    document.getElementById("welcome__button-muz").classList.remove("active");
     document.getElementById("welcome__body").classList.remove("active");
     document.getElementById("title-muz").classList.remove("active");
     document.getElementById("title-man").classList.remove("active");
     document.getElementById("text-muz").classList.remove("active");
     document.getElementById("text-man").classList.remove("active");
+    document.getElementById("header__stick").classList.remove("active");
+    document.getElementById("musicians__body").classList.remove("active");
+    document.getElementById("musicians").classList.remove("active");
 }
 
 document.querySelector('#musicians__body').oninput = function () {
